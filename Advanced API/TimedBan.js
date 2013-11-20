447a284d45f57c1f02911e05a9a776654dd90071
caleb <ramboxp@live.com>
2013-11-20 00:37:57 -0500

    Add timedBan.js

---
+++ Advanced API/TimedBan.js
@@ -0,0 +1,14 @@
+  //////////////////////////////////////////////////////
+ //!!THE UNBAN API FUNCTION IS BROKEN AT THE MOMENT!!//
+//////////////////////////////////////////////////////
+
+//We'll be doing this example in the form of a room bot
+//Create the function which will ban the user and set the timer to unban them
+function moderateTimedBan(who, time, reason) {
+	//Ban the user
+	API.moderateBanUser(who, reason);
+	//Define unban function to use in the timeout
+	function unban() { API.moderateUnbanUser(who) };
+	//Set the timeout long with a timeout ID so it can be cleared if needed
+	banTimer = setTimeout(unban, time * 60000) //We are using minutes for the time, so we multiply the time argument because timeout takes milliseconds
+}
