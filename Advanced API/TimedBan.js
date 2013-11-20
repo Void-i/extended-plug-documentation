9ec110cceccb5a7d84dd1ec2cb33259c7b4156af
derpthebass <ramboxp@live.com>
2013-11-20 00:42:59 -0500

    typo

--- Advanced API/TimedBan.js
+++ Advanced API/TimedBan.js
@@ -8,6 +8,6 @@ function moderateTimedBan(who, time, reason) {
 	API.moderateBanUser(who, reason);
 	//Define unban function to use in the timeout
 	function unban() { API.moderateUnbanUser(who) };
-	//Set the timeout long with a timeout ID so it can be cleared if needed
+	//Set the timeout with a timeout ID so it can be cleared if needed
 	banTimer = setTimeout(unban, time * 60000) //We are using minutes for the time, so we multiply the time argument because timeout takes milliseconds
 }
